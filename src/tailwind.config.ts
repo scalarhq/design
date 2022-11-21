64014ca1bb4065627c77c0b72116adcc45b7664d
Rahul Tarak <cryogenicplanet@gmail.com>
2022-11-21 16:08:06 +0530

    Brought over config from editor

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -1,4 +1,10 @@
 import { Config as TailwindConfig } from 'tailwindcss'
+import colors from 'tailwindcss/colors'
+
+import aspectRatio from '@tailwindcss/aspect-ratio'
+import forms from '@tailwindcss/forms'
+import lineClamp from '@tailwindcss/line-clamp'
+import typography from '@tailwindcss/typography'
 
 export const withModfyTailwindConfig = (
   config: TailwindConfig
@@ -13,13 +19,46 @@ export const withModfyTailwindConfig = (
         'midnightPurple-100': '#FBFDFE',
         midnightPurple: '#5850EC',
         'midnightPurple-500': '#5850EC',
-        'midnightPurple-900': '#060414'
+        'midnightPurple-900': '#060414',
+        orange: colors.orange,
+        teal: colors.teal,
+        blueGray: { ...colors.slate, 1000: '#0C1322' },
+        amber: colors.amber,
+        coolGray: colors.gray,
+        lime: colors.lime,
+        green: colors.emerald,
+        yellow: colors.amber,
+        purple: colors.violet
+      },
+      fontFamily: {
+        inter: ['Inter'],
+        bebasNeue: ['Bebas Neue', 'cursive'],
+        dmSans: ['DM Sans', 'sans-serif'],
+        ebGaramond: ['EB Garamond', 'serif'],
+        niconne: ['Niconne', 'cursive'],
+        spaceGrotesk: ['Space Grotesk', 'sans-serif'],
+        eudoxus: [`'Eudoxus Sans'`]
       }
     }
   }
 
+  const corePlugins: TailwindConfig['corePlugins'] = {
+    ...config.corePlugins,
+    aspectRatio: false
+  }
+
+  const plugins: TailwindConfig['plugins'] = [
+    ...(config.plugins || []),
+    forms,
+    aspectRatio,
+    lineClamp,
+    typography
+  ]
+
   return {
     ...config,
-    theme
+    theme,
+    corePlugins,
+    plugins
   }
 }
